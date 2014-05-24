37a932177013c289f3e11bbf0c653c346de25070
janice <janice@baileywx.com>
2014-05-24 18:18:03 -0400

    remove db stuff

--- db/migrate/20140211202006_add_badge_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddBadgeToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :badge, :integer
-    remove_column :users, :email, :string
-  end
-end
--- db/migrate/20140216202551_add_index_and_password_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexAndPasswordToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :password_digest, :string
-    add_column :users, :remember_token, :string
-    add_index  :users, :remember_token
-    add_index  :users, :badge, unique: true
-  end
-end
