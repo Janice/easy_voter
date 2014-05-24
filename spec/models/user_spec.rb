d403a3cd64b4fbd6c3d12123412671d1b78c4243
janice <janice@baileywx.com>
2014-05-24 18:55:29 -0400

    ugh

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", badge: "11111",
+                     password: "gators", password_confirmation: "gators")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:badge) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+    describe "when badge is not present" do
+    before { @user.badge = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", badge: "11111",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+end
